c02fd3d5d1707e8fc61240340140a1ca398a1a8c
Peet Klecha <p.a.klecha@gmail.com>
2019-10-22 15:25:23 -0400

    merge function

--- mergeSort.js
+++ mergeSort.js
@@ -1,4 +1,19 @@
 function split(wholeArray) {
-
+  const lenOfFirstHalf = Math.ceil(wholeArray.length/2)
+  const firstHalf = wholeArray.slice(0,lenOfFirstHalf)
+  const secondHalf = wholeArray.slice(lenOfFirstHalf)
   return [firstHalf, secondHalf];
 }
+
+function merge(arr1, arr2){
+  const combinedArray = []
+  while(arr1.length && arr2.length){
+    if (arr1[0] < arr2[0]){
+      combinedArray.push(arr1.shift())
+    } else combinedArray.push(arr2.shift())
+  }
+  if (arr1.length) combinedArray.push(...arr1)
+  if (arr2.length) combinedArray.push(...arr2)
+  return combinedArray
+}
+
--- mergeSort.spec.js
+++ mergeSort.spec.js
@@ -6,3 +6,9 @@ describe('Split Array function', function() {
     expect(split([1,2,3,4,5])).toEqual([[1,2,3],[4,5]])
   });
 });
+
+describe('merge function', function() {
+  it('merges two sorted arrays into one sorted array', function() {
+    expect(merge([1,3],[2,4])).toEqual([1,2,3,4])
+  });
+});
