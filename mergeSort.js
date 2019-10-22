197b341d1b051a73128be679c5552cb9d73f6fa2
Redion <rkadilliu@gmail.com>
2019-10-22 15:52:46 -0400

    solved

--- mergeSort.js
+++ mergeSort.js
@@ -1,19 +1,32 @@
 function split(wholeArray) {
-  const lenOfFirstHalf = Math.ceil(wholeArray.length/2)
-  const firstHalf = wholeArray.slice(0,lenOfFirstHalf)
-  const secondHalf = wholeArray.slice(lenOfFirstHalf)
+  const lenOfFirstHalf = Math.ceil(wholeArray.length / 2);
+  const firstHalf = wholeArray.slice(0, lenOfFirstHalf);
+  const secondHalf = wholeArray.slice(lenOfFirstHalf);
   return [firstHalf, secondHalf];
 }
 
-function merge(arr1, arr2){
-  const combinedArray = []
-  while(arr1.length && arr2.length){
-    if (arr1[0] < arr2[0]){
-      combinedArray.push(arr1.shift())
-    } else combinedArray.push(arr2.shift())
+function merge(arr1, arr2) {
+  const combinedArray = [];
+  while (arr1.length && arr2.length) {
+    if (arr1[0] < arr2[0]) {
+      combinedArray.push(arr1.shift());
+    } else combinedArray.push(arr2.shift());
   }
-  if (arr1.length) combinedArray.push(...arr1)
-  if (arr2.length) combinedArray.push(...arr2)
-  return combinedArray
+  if (arr1.length) combinedArray.push(...arr1);
+  if (arr2.length) combinedArray.push(...arr2);
+  return combinedArray;
 }
 
+function mergeSort(array) {
+  /* your code here */
+  if (array.length < 2) {
+    return array;
+  }
+
+  let splitArr = split(array);
+  let firstSplitArr = mergeSort(splitArr[0]);
+  console.log("first", firstSplitArr);
+  let secondSplitArr = mergeSort(splitArr[1]);
+  console.log("second", secondSplitArr);
+  return merge(firstSplitArr, secondSplitArr);
+}
--- mergeSort.spec.js
+++ mergeSort.spec.js
@@ -1,14 +1,20 @@
-describe('Split Array function', function() {
-  it('is able to split an array into two halves', function() {
-    expect(split([1,2])).toEqual([[1],[2]])
+describe("Split Array function", function() {
+  it("is able to split an array into two halves", function() {
+    expect(split([1, 2])).toEqual([[1], [2]]);
   });
-  it('is able to split an array into two halves', function() {
-    expect(split([1,2,3,4,5])).toEqual([[1,2,3],[4,5]])
+  it("is able to split an array into two halves", function() {
+    expect(split([1, 2, 3, 4, 5])).toEqual([[1, 2, 3], [4, 5]]);
   });
 });
 
-describe('merge function', function() {
-  it('merges two sorted arrays into one sorted array', function() {
-    expect(merge([1,3],[2,4])).toEqual([1,2,3,4])
+describe("merge function", function() {
+  it("merges two sorted arrays into one sorted array", function() {
+    expect(merge([1, 3], [2, 4])).toEqual([1, 2, 3, 4]);
+  });
+});
+
+describe("mergeSort function", function() {
+  it("return sorted array", function() {
+    expect(mergeSort([1, 3, 2, 4, 5])).toEqual([1, 2, 3, 4, 5]);
   });
 });
